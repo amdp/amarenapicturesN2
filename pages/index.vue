d506866a025f5395f60bff5ba3425a2dde6baed1
Alessandro Merletti de Palo <alessandromerlettidepalo@gmail.com>
2020-01-25 15:03:26 +0100

    Update index.vue
    small talks :)

--- pages/index.vue
+++ pages/index.vue
@@ -15,8 +15,9 @@
           "<span class="amarenared">sour cherry</span>" production service for
           our directions, in 2010. Today it has become a full production company
           leveraging on our diection style and its unique
-          <span class="amarenared">flavour</span>.
+          <span class="amarenared">flavour</span>. We're just two simple
         </em>
+        <span class="amarenared">videogardeners</span>.
       </b-col>
     </b-row>
     <b-row class="d-flex justify-content-center mx-3">
