6fd4153abb75761fd4ddc77cbcdfc3ff40cb22bc
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2020-01-31 16:35:07 +0100

    font preloading

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,12 +50,12 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css?family=Oswald:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap'
-      }
     ]
+  },
+  webfontloader: {
+    google: {
+      families: ['Oswald:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap']
+    }
   },
   auth: {
     strategies: {
