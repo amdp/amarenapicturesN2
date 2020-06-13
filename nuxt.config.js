098ac80de156f727b3a464c97f7f12cc1c7df0c8
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2020-06-13 21:43:32 +0200

    fix font

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,29 +40,6 @@ module.exports = {
       }
     }
     : { host: process.env.HOST, port: process.env.PORT },
-  head: {
-    title: 'Amarena Pictures',
-    meta: [
-      { charset: 'utf-8' },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
-      },
-      { hid: 'description', name: 'description', content: pkg.description },
-      { property: 'og:desc', content: 'That\'s Amarena! Founded by directors Alessandro Merletti De Palo and Giovanni Caloro, Amarena Pictures combines the art of film direction with solid, cutting-edge production.' },
-      { property: 'og:title', content: 'Amarena Pictures' },
-      { property: 'og:image', content: '@assets/amarenasquare.png' },
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'preload',
-        href:
-          'https://fonts.googleapis.com/css?family=Oswald:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap',
-        as: "style",
-      },
-    ]
-  },
   i18n: {
     locales: ['en', 'it'],
     defaultLocale: 'en',
