04bee318721bb63cbf497b6dd8ca5e2b35069a54
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2020-06-13 21:33:17 +0200

    font fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,7 +58,8 @@ module.exports = {
       {
         rel: 'preload',
         href:
-          'https://fonts.googleapis.com/css?family=Oswald:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap'
+          'https://fonts.googleapis.com/css?family=Oswald:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap',
+        as: "style",
       },
     ]
   },
