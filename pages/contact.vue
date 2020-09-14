ea16960c4124e193a7b230e4404fc699de7f2de4
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2020-09-14 12:11:54 +0200

    minor fixes on button

--- pages/contact.vue
+++ pages/contact.vue
@@ -105,14 +105,12 @@
               @click="cancel()"
               v-html="$t('contact.cancel')"
             >
-            </b-button
-            ><br /><br />
+            </b-button>
             <b-check id="termscheckbox" name="termscheckbox" required>
               <span class="diversity">
                 <i v-html="$t('contact.checkbox')"> </i>
               </span>
             </b-check>
-            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
             <div v-if="response.length" v-html="response"></div>
           </b-col>
         </b-row>
