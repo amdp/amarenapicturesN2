1bb276b3cf1fafb7610415ad6de154b5030a8347
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2021-10-12 13:52:58 +0200

    whyme

--- pages/whyme.vue
+++ pages/whyme.vue
@@ -6,19 +6,29 @@
       <b-col md="6" cols="10">
         <b-row class="m-0 p-0">
           <b-col cols="12">
-            <p class="amarenared t32 text-center">
+            <p class="amarenared t32 text-center" @click="go()">
               WHY ME - ANTEPRIMA DOCUMENTARIO
             </p>
+            <p class="t18 text-center">FOR YOUR EYES ONLY</p>
           </b-col>
         </b-row>
-        <b-container class="m-0 p-0 text-justify">
-          <p class="t18 text-center">FOR YOUR EYES ONLY</p>
-          <b-container class="m-0 p-0 pointer">
-            <b-embed type="video" autoplay controls>
-              <source src="../assets/thatsamarena.mp4" type="video/mp4" />
-            </b-embed>
-          </b-container>
-        </b-container>
+        <b-row class="m-0 p-0">
+          <b-col cols="12">
+            <b-container class="m-0 p-0">
+              <video
+                type="video"
+                autoplay
+                controls
+                style="position: absolute; top: 0px; left: 0px; width: 100%"
+                id="whymevid"
+                name="whymevid"
+              >
+                <source src="/v/whyme.mp4#t=2" type="video/mp4" />
+              </video>
+              <br /><br /><br /><br /><br /><br /><br />
+            </b-container>
+          </b-col>
+        </b-row>
       </b-col>
       <b-col md="3" cols="1"></b-col>
     </b-row>
@@ -32,6 +42,6 @@ export default {
   async fetch({ store }) {
     await store.dispatch('getVideoAction')
     await store.dispatch('getBrandAction')
-  },
+  }
 }
 </script>
